56c560140367e955f1fe93487fcd4b30c9ad4e2e
Mohammed Kharoda <kharodawalam@gmail.com>
2023-08-18 14:48:00 +0530

    first commit

---
+++ app/components/Logo.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { ImageConfig } from "@/utils/ImageConfig";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+const Logo = () => {
+  const router = useRouter();
+  return (
+    <>
+      <Image
+        onClick={() => router.push("/")}
+        alt="logo"
+        className="hidden md:block cursor-pointer"
+        height="100"
+        width="100"
+        src={ImageConfig.AIRBNB_LOGO}
+      />
+    </>
+  );
+};
+
+export default Logo;
---
+++ utils/ImageConfig.ts
@@ -0,0 +1,3 @@
+export const ImageConfig = {
+    AIRBNB_LOGO:'/images/airbnb.svg'
+}
