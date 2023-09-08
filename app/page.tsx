7046ed98031ad3033193ec65f96be18f29b31c30
Mohammed Kharoda <kharodwalam@gmail.com>
2023-09-08 21:35:27 +0530

    fix page-render

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,52 @@
-import Container from "@/src/common/Container";
+export const dynamic = "force-dynamic";
+
+import getCurrentUser from "@/actions/getCurrentUser";
 import ClientOnly from "./ClientOnly";
-import EmptyState from "@/src/components/EmptyState";
-import getListing, { ILisitngParams } from "./actions/getListings";
-import ListingCard from "@/src/components/ListingCard";
-import getCurrentUser from "./actions/getCurrentUser";
+import getListings, { IListingsParams } from "@/actions/getListings";
+import Container from "@/components/Container";
+import EmptyState from "@/components/EmptyState";
+import ListinCard from "@/components/listings/ListinCard";
 
 interface HomeProps {
-  searchParams:ILisitngParams
+  searchParams: IListingsParams;
 }
 
-export const dynamic = 'force-dynamic'
-
-export default async function Home({ searchParams }: HomeProps) {
-  const listings = await getListing(searchParams);
+const Home = async ({ searchParams }: HomeProps) => {
+  const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
-
   if (listings.length === 0) {
     return (
-      <ClientOnly>
-        <EmptyState showReset />
-      </ClientOnly>
-    );
+<ClientOnly>
+<EmptyState showReset />
+<ClientOnly/>
+);
   }
   return (
-    <>
-      <ClientOnly>
-        <Container>
-          <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-            {listings.map((listings: any) => {
-              return (
-                <ListingCard
-                  disable
-                  currentUser={currentUser}
-                  key={listings.id}
-                  data={listings}
-                />
-              );
-            })}
-          </div>
-        </Container>
-      </ClientOnly>
-    </>
+    <div>
+      <Container>
+        <div
+          className="pt-24
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8"
+        >
+          {listings.map((listing) => {
+            return (
+              <ListinCard
+                currentUser={currentUser}
+                key={listing.id}
+                data={listing}
+              />
+            );
+          })}
+        </div>
+      </Container>
+    </div>
   );
-}
+};
+export default Home;
